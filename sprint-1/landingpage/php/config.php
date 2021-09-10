1f7f9ea7f130fe5f290150c480a86dd6e0aab3fb
Thiago Aires <thiagoaires.o@gmail.com>
2021-09-10 09:29:38 -0300

    minor update

--- sprint-1/landingpage/php/config.php
+++ sprint-1/landingpage/php/config.php
@@ -1,15 +1,17 @@
 <?php
-
+/*
 define('DBSERVER', 'localhost'); // Data server
 define('DBUSERNAME', 'root'); // Database username
 define('DBPASSWORD', 'Thiago270602'); // Database password
 define('DBNAME', 'world'); // Database name
-
+*/
 /*connect to MySQL database */
-$db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+$db = mysqli_connect("localhost", "root", "Thiago270602", "world");
 
 //Check db connection
 if($db === false){
     die("Error: connection error. ". mysqli_connect_error());
 }
+
+mysqli_close($db);
 ?>
