1f618328132d6e02aabf09c734704449aa60295c
Thiago Aires <thiagoaires.o@gmail.com>
2021-09-10 11:47:50 -0300

    Update config.php

--- sprint-1/landingpage/php/config.php
+++ sprint-1/landingpage/php/config.php
@@ -2,16 +2,28 @@
 /*
 define('DBSERVER', 'localhost'); // Data server
 define('DBUSERNAME', 'root'); // Database username
-define('DBPASSWORD', 'Thiago270602'); // Database password
-define('DBNAME', 'world'); // Database name
-*/
+define('DBPASSWORD', ''); // Database password
+define('DBNAME', 'demo'); // Database name
+
 /*connect to MySQL database */
-$db = mysqli_connect("localhost", "root", "Thiago270602", "world");
+/*
+$db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
 
 //Check db connection
 if($db === false){
-    die("Error: connection error. ". mysqli_connect_error());
+    die("Error: connection error. " . mysqli_connect_error());
 }
+*/
+$link = mysqli_connect("localhost", "root", "Thiago270602", "world");
+
+if ($link) {
+    echo "Erro: Falha ao conectar-se com o banco de dados." . PHP_EOL;
+    echo "Debugging: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging: " . mysqli_connect_errno() . PHP_EOL;
+    exit;
+}
+
+echo "sucesso: Sucesso ao conectar-se com a base com a base de dados MySQL" .  PHP_EOL;
 
 mysqli_close($db);
 ?>
